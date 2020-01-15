af3635be52308a99aecf4de72a575310b7989b6e
Botzen16 <59736857+Botzen16@users.noreply.github.com>
2020-01-15 09:23:16 -0500

    Add files via upload

--- teamNA.py
+++ teamNA.py
@@ -11,16 +11,11 @@ strategy_name = 'Collude'
 strategy_description = 'Always collude.'
     
 def move(my_history, their_history, my_score, their_score):
-    '''Make my move based on the history with this player.
-    
-    history: a string with one letter (c or b) per round that has been played with this opponent.
-    their_history: a string of the same length as history, possibly empty. 
-    The first round between these two players is my_history[0] and their_history[0]
-    The most recent round is my_history[-1] and their_history[-1]
-    
-    Returns 'c' or 'b' for collude or betray.
-    '''
-    
-    # This player always colludes.
-    return 'c'
+    if len(my_history)==0:
+        return 'd'
+    elif their_history[-1]=='d'and their_history[-2]=='d':
+        return 'd'
+    else:
+        return 'c'
+        
     
