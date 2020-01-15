bb92be280964fc814d71266992e3edcc72ab2b99
Yoyomaster35 <59736521+Yoyomaster35@users.noreply.github.com>
2020-01-15 09:35:31 -0500

    Add files via upload

--- team mahjong.py
+++ team mahjong.py
@@ -6,9 +6,11 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
+team_name = 'Mahjong' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_description = '''start by betraying the other prisoner for several
+        turns, and then view their history. if they collude a lot, then you
+        continue to betray. otherwise, collude.'''
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -25,10 +27,12 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
-    
+    for i in range(number_of_rounds):
+        if my_score <= 0:
+            return 'b'
+        else:
+            return 'c'
+                
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
